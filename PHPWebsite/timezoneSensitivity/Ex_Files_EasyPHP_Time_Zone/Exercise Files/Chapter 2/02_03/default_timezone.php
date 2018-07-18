761eba996872a236c9823926b6a45620c6100e0c
Natalie Chappelhow <Natalie.Chappelhow@nublue.co.uk>
2018-07-18 10:04:54 +0100

    starting to create a login form/page

--- PHPWebsite/timezoneSensitivity/Ex_Files_EasyPHP_Time_Zone/Exercise Files/Chapter 2/02_03/default_timezone.php
+++
@@ -1,7 +0,0 @@
-<?php
-  
-date_default_timezone_set('America/Los_Angeles');
-
-echo date_default_timezone_get();
-
-?>
---
+++ login/login.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<title>PHP Practice - Creating a login page</title>
+</head>
+<body>
+	
+
+	
+	<div class="container">
+		<div class="form">
+			<form name="loginForm" id="loginForm" action = "" method = "get">
+				<h2>Form</h2>
+				<label for="length">Length:</label>
+				<input type="text" id="length" name="length" value="" /> 
+				
+				<label for="lowerCase">lowerCase:</label>
+				<input type="text" id="lowerCase" name="lowerCase" value="" />
+
+				<input type="submit" value="Submit">
+
+			</form>
+		</div>
+	</div>
+</body>
+</html>
